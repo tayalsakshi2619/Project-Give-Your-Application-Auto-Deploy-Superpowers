d0181ab2ab9c99436f0cab17199c9ab83d067804
tayalsakshi381 <53163847+tayalsakshi381@users.noreply.github.com>
2021-06-26 12:38:06 +0530

    Fixed the error

--- frontend/src/app/containers/Employee/services/employees.ts
+++ frontend/src/app/containers/Employee/services/employees.ts
@@ -1,4 +1,4 @@
-import { HttpService } from '../../../services/httpService';,
+import { HttpService } from '../../../services/httpService';
 import { EmployeeModel,
   EmployeeUpdateStringFieldModel,
   EmployeeUpdateDateFieldModel,
